7912780a9d0275ab37a727792b268a1b25ac32ab
MiraDiv-git <omega24122004@gmail.com>
2025-11-18 18:25:23 +0200

    Added autocommit script

---
+++ Instruments/GitHub/commit_and_push.py
@@ -0,0 +1,12 @@
+import os
+
+msg = input("Commit message: ")
+
+os.system("git add .")
+os.system(f'git commit -m "{msg}"')
+
+if input("Continue? (y/n): ").strip().lower() == 'y':
+    os.system("git push -u origin main")
+    print("[!] Done")
+else:
+    print("[!] Aborted by user")
